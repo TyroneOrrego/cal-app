82931a5f6ff9cd0a3a6c5e312147eb090181aa7c
Tyrone <steven.orrego93@gmail.com>
2022-03-07 17:24:04 -0500

    Final release

---
+++ src/components/Result.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type Prop = {
+  value: string;
+};
+
+const Result: FC<Prop> = ({ value }) => <div className="result">{value}</div>;
+
+Result.defaultProps = {
+  value: "0",
+};
+
+export default Result;
